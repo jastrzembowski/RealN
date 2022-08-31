547e8ba1c69445bbc0c5fb499644c2f98c394363
jastrzembowski <jastrzembowskikamil@gmail.com>
2022-08-31 16:21:11 +0200

    bugfixes

--- src/components/buy/whybuy/WhyBuy.tsx
+++ src/components/buy/whybuy/WhyBuy.tsx
@@ -4,7 +4,6 @@ import "./whybuy.scss";
 import mock from "../../../images/mock.png";
 import getit from "../../../images/getit.jpg";
 import pic2 from "../../../images/pic2.jpg";
-import {Link} from "react-router-dom"
 
 const whybuy = () => {
   return (
@@ -15,19 +14,27 @@ const whybuy = () => {
         <h1>Dlaczego warto kupić właśnie u nas?</h1>
         <ul>
           <li>
-            <p>Z nami znajdziesz wymarzoną nieruchomość, już dziś umów się na prezentację!
-</p>Aktualizujemy oferty co 5 minut więc możesz wyprzedzić innych kupujących 
-poprzez natychmiastową rezerwację prezentacji z Agentem RealN.
-
+            <p>
+              Z nami znajdziesz wymarzoną nieruchomość, już dziś umów się na
+              prezentację!
+            </p>
+            Aktualizujemy oferty co 5 minut więc możesz wyprzedzić innych
+            kupujących poprzez natychmiastową rezerwację prezentacji z Agentem
+            RealN.
           </li>
           <li>
-            <p> Lokalny agent RealN jako Twój przewodnik </p>Twój Agent pomoże Ci w najlepszy sposób przejść przez proces negocjacji, bazując na jego wieloletnim doświdczeniu-
-Z nami poczujesz się bezpiecznie dokonując właściwego wyboru.
-
+            <p> Lokalny agent RealN jako Twój przewodnik </p>Twój Agent pomoże
+            Ci w najlepszy sposób przejść przez proces negocjacji, bazując na
+            jego wieloletnim doświdczeniu- Z nami poczujesz się bezpiecznie
+            dokonując właściwego wyboru.
           </li>
           <li>
-            <p> Bez prowizji </p><span className="text-bold">Nie pobieramy prowizji od strony Kupującej</span> dzięki naszej wyjątkowej technologii, wyróżniamy się dużą skutecznością w działaniach.
-
+            <p> Bez prowizji </p>
+            <span className="text-bold">
+              Nie pobieramy prowizji od strony Kupującej
+            </span>{" "}
+            dzięki naszej wyjątkowej technologii, wyróżniamy się dużą
+            skutecznością w działaniach.
           </li>{" "}
         </ul>
       </article>
@@ -37,34 +44,34 @@ Z nami poczujesz się bezpiecznie dokonując właściwego wyboru.
         <h1>Jak to działa?</h1>
         <ul>
           <li>
-            <p>Bądź na bieżąco, tak aby najlepsze Cię nie ominęło.
-</p> Zwiedzanie za pośrednictwem naszej strony i aplikacji jest łatwe. 
-Zapisz wyszukiwanie w swoich ulubionych dzielnicach, aby otrzymywać powiadomienia o nowych domach.
-
+            <p>Bądź na bieżąco, tak aby najlepsze Cię nie ominęło.</p>{" "}
+            Zwiedzanie za pośrednictwem naszej strony i aplikacji jest łatwe.
+            Zapisz wyszukiwanie w swoich ulubionych dzielnicach, aby otrzymywać
+            powiadomienia o nowych domach.
           </li>
           <li>
-            <p>Spotkanie z Agentem RealN-zaoszczędzisz z nami swój czas
-</p>Na podstawie szczegółowego wywiadu i określeniu 6 najważniejszych cech dotyczących Twojej wymarzonej nieruchomości
-Twój Agent rozpocznie przygotowanie idealnie dopasowanych ofert.
-
+            <p>Spotkanie z Agentem RealN-zaoszczędzisz z nami swój czas</p>Na
+            podstawie szczegółowego wywiadu i określeniu 6 najważniejszych cech
+            dotyczących Twojej wymarzonej nieruchomości Twój Agent rozpocznie
+            przygotowanie idealnie dopasowanych ofert.
           </li>
           <li>
-            <p> Złóż ofertę i w krótkim czasie odbierz klucze od Twojego nowego M.
-</p>W prosty i bezpieczny sposób pomożemy przejść Ci ten proces.
-
+            <p>
+              {" "}
+              Złóż ofertę i w krótkim czasie odbierz klucze od Twojego nowego M.
+            </p>
+            W prosty i bezpieczny sposób pomożemy przejść Ci ten proces.
           </li>
         </ul>
       </article>
       <article className="mockup-holder">
         <img src={mock} alt="site mockup"></img>
         <aside>
-          <h1>Zapraszamy na prezentacje 
-</h1>
+          <h1>Zapraszamy na prezentacje</h1>
           <p>
-          Aktualizujemy oferty co 5 minut, więc możesz wyprzedzić 
-innych kupujących, natychmiast umawiając się na wycieczkę 
-z agentem RealN, osobiście lub przez wideo-czat.
-
+            Aktualizujemy oferty co 5 minut, więc możesz wyprzedzić innych
+            kupujących, natychmiast umawiając się na wycieczkę z agentem RealN,
+            osobiście lub przez wideo-czat.
           </p>
           <img src={getit} alt="store img"></img>
         </aside>
@@ -72,28 +79,19 @@ z agentem RealN, osobiście lub przez wideo-czat.
       <article className="ready-holder">
         <img src={pic2} alt="home to buys"></img>
         <aside>
-          <h1>Sprzedajesz swój stary dom lub mieszkanie?
-</h1>
-<h2>
-Mamy dla Ciebie propozycję..
-
-</h2>
+          <h1>Sprzedajesz swój stary dom lub mieszkanie?</h1>
+          <h2>Mamy dla Ciebie propozycję..</h2>
           <p>
-          Zapłacisz tylko 1% prowizji, kiedy kupujesz i sprzedajesz z nami.
+            Zapłacisz tylko 1% prowizji, kiedy kupujesz i sprzedajesz z nami.
           </p>
-          <p>
-          Dzięki narzędziom takim jak:
-          </p>
+          <p>Dzięki narzędziom takim jak:</p>
           <ul>
             <li>spacery 3D</li>
-            <li>
-              prezentacje live, wideo
-            </li>
-            <li>
-              cyfrowe kampanie marketingowe
-            </li>
+            <li>prezentacje live, wideo</li>
+            <li>cyfrowe kampanie marketingowe</li>
           </ul>
-<h1>Jesteśmy skuteczni</h1>   </aside>
+          <h1>Jesteśmy skuteczni</h1>{" "}
+        </aside>
       </article>
     </>
   );
